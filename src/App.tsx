c4c536a7baa58193a4e550183fcee60846513288
satake <satake@onconnected.onmicrosoft.com>
2022-09-07 15:46:43 +0900

    axios install

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -15,11 +16,24 @@ function App() {
        console.log(err);
        console.log("err");
      });
- }, []);
+  }, []);
+  const getData = async () => {
+    try {
+      const result = await axios.get('/api/albums')
+      console.log("🚀 ~ file: App.tsx ~ line 23 ~ getData ~ result.data()", result.data)  
+    } catch (error) {
+      console.log("🚀 ~ file: App.tsx ~ line 26 ~ getData ~ error", error)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>NewOne</h1>
+        <button
+          onClick={getData}
+        >
+          axios.get()
+        </button>
       </header>
     </div>
   );
